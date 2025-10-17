0d311d61d33324075460d46d099c6968c2efe061
rakasyaaahh <farrelrakasyah@gmail.com>
2025-10-17 08:51:42 +0700

    UTS_ALPRO1D_25-113_HawwinFarrelRakasyah

---
+++ UTS_ALPRO1D_25-113_HawwinFarrelRakasyah/UTS_ALPRO1D_25-113_HawwinFarrelRakasyah/uts.py
@@ -0,0 +1,15 @@
+#Rental Motor Aconk
+motor_matic = 50000
+motor_trail = 100000
+motor_sport = 75000
+asuransi = 15000
+totalharga=0
+
+jenismotor =(input("Pilih jenis motor :"))
+lamasewa = int(input("Masukkan lama sewa :"))
+if lamasewa >3
+    totalharga =jenismotor + asuransi
+elif totalharga > 150000
+    totalharga - 0.10
+input ("Apakah anda memiliki kupon?")
+
